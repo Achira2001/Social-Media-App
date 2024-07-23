489bda3cc8fcbc3a8106bb359d50cd5a7da36f86
Achira2001 <achira.20220024@iit.ac.lk>
2024-07-23 06:20:29 +0530

    Update files

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
-      404 | NotFound
+    <div className = "position-relative" style={{minWidth: 'calc(100vh - 70px)'}} >
+      <h2 className="position-absolute text-secondary" 
+      style={{top: '50%', left:50%, transform:'translate(-50%, -50%)'}}>
+        404 | Not Found.
+      </h2>
     </div>
   )
 }
