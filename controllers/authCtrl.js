33e45edf778c09b4db03e5a543e9ea13a52789d9
Achira2001 <achira.20220024@iit.ac.lk>
2024-07-23 23:50:21 +0530

    update files

---
+++ controllers/authCtrl.js
@@ -0,0 +1,38 @@
+const User = require('..models/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const authCtrl = {
+    register: async (req, res) => {
+        try {
+
+        }catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    
+    },
+    login: async (req, res) => {
+        try {
+
+        }catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    
+    },
+    logout: async (req, res) => {
+        try {
+
+        }catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    
+    },
+    generateAccessToken: async (req, res) => {
+        try {
+
+        }catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    
+    }
+}
--- models/userModel.js
+++ models/userModel.js
@@ -14,12 +14,42 @@ const userSchema = new mongoose.Schema({
         maxlength: 25,
         unique: true
     },
-    username: {
+    email: {
         type: String,
         required: true,
         trim: true,
         unique: true
-    }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
+    },
+    role: {type: String, default: 'user'},
+    gender: {type: String, default: 'male'},
+    mobile: {type: String, default: ''},
+    address: {type: String, default: ''},
+    story: {
+        type: String, 
+        default: '',
+        maxlength: 200
+    },
+    website: {type: String, default: ''},
+    followers: [
+        {
+            type: mongoose.Types.objectId, 
+            ref: ''
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Types.objectId, 
+            ref: ''
+        }
+    ]
 }, {
     timestamps: true
 })
