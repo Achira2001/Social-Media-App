b9e8e64fe56db28c12d889b1cb89d4ebfc1a5af5
Achira2001 <achira.20220024@iit.ac.lk>
2024-07-27 20:42:03 +0530

    update files

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -85,7 +85,8 @@ const authCtrl = {
     },
     logout: async (req, res) => {
         try {
-            res.clearCookie('refreshtoken')
+            res.clearCookie('refreshtoken', {path: '/api/refresh_token'})
+            return res.json({msg: "Logged Out!"})
         } catch (err) {
             return res.status(500).json({ msg: err.message });
         }
